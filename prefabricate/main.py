bfd0c4374bc3e58094b308d390d8b7d3432e7067
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-17 14:04:28 -0500

    updated version to 0.0.4

--- prefabricate/main.py
+++ prefabricate/main.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+import os
+
 
 def main():
     """
@@ -7,7 +9,8 @@ def main():
     parser = ArgumentParser("Run a prefabrication configuration file")
     parser.add_argument('config_file', metavar="configfile", nargs=1, help="Configuration File for prefabrication")
     args = parser.parse_args()
-    print args.config_file
-    with open(args.config_file, 'r') as f:
+    blueprint = os.path.join(os.getcwd(), args.config_file[0])
+    print blueprint
+    with open(blueprint, 'r') as f:
         contents = f.read()
     return "Confirguration:\n %s" % contents
