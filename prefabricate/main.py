39ca6fd163987211b02dc9db58dfe13f1a10cf11
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-12-17 13:46:52 -0500

    fixed ngargs

--- prefabricate/main.py
+++ prefabricate/main.py
@@ -1,9 +1,12 @@
-import os
-from optparse import OptionParser
-
+from argparse import ArgumentParser
 
 def main():
     """
     Main Loop for django fabrication
     """
-    return "hello world"
+    parser = ArgumentParser("Run a prefabrication configuration file")
+    parser.add_argument('config_file', metavar="configfile", nargs=1, help="Configuration File for prefabrication")
+    config_file = parser.parse_args()
+    with open(config_file, 'r') as f:
+        contents = f.read()
+    return "Confirguration:\n %s" % contents
